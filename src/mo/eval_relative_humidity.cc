0d476c79cfc9e3d383de26169a59245d034bc239
Francois Hebert <hebert@ucar.edu>
2025-10-27 19:04:02 -0700

    Enable multithreading in mo/eval_relative_humidity_at_2m_from_temp (#324)

--- src/mo/eval_relative_humidity.cc
+++ src/mo/eval_relative_humidity.cc
@@ -16,12 +16,16 @@
 #include "mo/eval_relative_humidity.h"
 #include "mo/eval_sat_vapour_pressure.h"
 
+#include "oops/util/for_each.h"
 #include "oops/util/FunctionSpaceHelpers.h"
 #include "oops/util/Logger.h"
 
 using atlas::array::make_view;
 using atlas::idx_t;
 using atlas::util::Config;
+
+using View = atlas::array::LocalView<double, 1>;
+using ConstView = atlas::array::LocalView<const double, 1>;
 
 namespace {
   const char specific_humidity_mo[] = "water_vapor_mixing_ratio_wrt_moist_air_and_condensed_water";
@@ -193,19 +197,22 @@ void eval_relative_humidity_at_2m_from_temp_tl(atlas::FieldSet & incFlds,
 
   // State variables qsat and dlsvpdT can be calculated by eval_sat_vapour_pressure_nl
   // and evalSatSpecificHumidity from air_temperature and air_pressure inputs
-  const auto qView = make_view<double, 2>(stateFlds[specific_humidity_mo]);
-  const auto qsatView = make_view<double, 2>(stateFlds["qsat"]);
-  const auto dlsvpdtView = make_view<double, 2>(stateFlds["dlsvpdT"]);
-  const auto qIncView = make_view<double, 2>(incFlds[specific_humidity_mo]);
-  const auto tIncView = make_view<double, 2>(incFlds["air_temperature_at_2m"]);
-  auto rh2mIncView = make_view<double, 2>(incFlds["relative_humidity_at_2m"]);
-  const idx_t sizeOwned =
-    util::getSizeOwned(incFlds["relative_humidity_at_2m"].functionspace());
+  util::for_each_column(
+    [=] (ConstView q,
+         ConstView qsat,
+         ConstView dlsvpdt,
+         ConstView qInc,
+         ConstView tInc,
+         View rh2mInc) {
+      rh2mInc(0) = 100.0 * (qInc(0) - q(0) * dlsvpdt(0) * tInc(0)) / qsat(0);
+    },
+    stateFlds[specific_humidity_mo],
+    stateFlds["qsat"],
+    stateFlds["dlsvpdT"],
+    incFlds[specific_humidity_mo],
+    incFlds["air_temperature_at_2m"],
+    incFlds["relative_humidity_at_2m"]);
 
-  for (atlas::idx_t jn = 0; jn < sizeOwned; ++jn) {
-      rh2mIncView(jn, 0) = 100.0 * (qIncView(jn, 0) - qView(jn, 0) *
-                                    dlsvpdtView(jn, 0) * tIncView(jn, 0)) / qsatView(jn, 0);
-  }
   incFlds["relative_humidity_at_2m"].set_dirty();
 
   oops::Log::trace()<< "[eval_relative_humidity_at_2m_from_temp_tl()] ... exit" << std::endl;
@@ -219,21 +226,23 @@ void eval_relative_humidity_at_2m_from_temp_ad(atlas::FieldSet & hatFlds,
 
   // State variables qsat and dlsvpdT can be calculated by eval_sat_vapour_pressure_nl
   // and evalSatSpecificHumidity from air_temperature and air_pressure inputs
-  const auto qView = make_view<double, 2>(stateFlds[specific_humidity_mo]);
-  const auto qsatView = make_view<double, 2>(stateFlds["qsat"]);
-  const auto dlsvpdtView = make_view<double, 2>(stateFlds["dlsvpdT"]);
-  auto qHatView = make_view<double, 2>(hatFlds[specific_humidity_mo]);
-  auto tHatView = make_view<double, 2>(hatFlds["air_temperature_at_2m"]);
-  auto rh2mHatView = make_view<double, 2>(hatFlds["relative_humidity_at_2m"]);
-  const idx_t sizeOwned =
-    util::getSizeOwned(hatFlds["relative_humidity_at_2m"].functionspace());
-
-  for (atlas::idx_t jn = 0; jn < sizeOwned; ++jn) {
-    qHatView(jn, 0) += 100 * rh2mHatView(jn, 0) / qsatView(jn, 0);
-    tHatView(jn, 0) -= 100 * qView(jn, 0) * dlsvpdtView(jn, 0) *
-      rh2mHatView(jn, 0) / qsatView(jn, 0);
-    rh2mHatView(jn, 0) = 0.0;
-  }
+  util::for_each_column(
+    [=] (ConstView q,
+         ConstView qsat,
+         ConstView dlsvpdt,
+         View qHat,
+         View tHat,
+         View rh2mHat) {
+      qHat(0) += 100 * rh2mHat(0) / qsat(0);
+      tHat(0) -= 100 * q(0) * dlsvpdt(0) * rh2mHat(0) / qsat(0);
+      rh2mHat(0) = 0.0;
+    },
+    stateFlds[specific_humidity_mo],
+    stateFlds["qsat"],
+    stateFlds["dlsvpdT"],
+    hatFlds[specific_humidity_mo],
+    hatFlds["air_temperature_at_2m"],
+    hatFlds["relative_humidity_at_2m"]);
 
   hatFlds[specific_humidity_mo].set_dirty();
   hatFlds["air_temperature_at_2m"].set_dirty();
